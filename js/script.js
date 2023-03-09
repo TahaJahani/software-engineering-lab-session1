e6eaae5ffaa0c39d24554b3838d45e72970fdcfe
taha7900 <taha7900@gmail.com>
2023-03-09 15:39:37 +0330

    Add click action function for numbers

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const outputTextArea = document.getElementById("calc-result")
+const buttonClicked = (text) => {
+    const outputTextArea = document.getElementById("calc-result")
+    outputTextArea.textContent += text
+}
