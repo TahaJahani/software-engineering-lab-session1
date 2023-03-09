feb79b9eb9ff329df00d83e1d0ee6720357d6446
taha7900 <taha7900@gmail.com>
2023-03-09 15:52:08 +0330

    Prevent double operator insertion

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 const outputTextArea = document.getElementById("calc-result")
+
+const isOperator = (char) => {
+    const operators = ["*", "+", "-", "%", "/"]
+    return operators.includes(char)
+}
+
 const buttonClicked = (text) => {
     const outputTextArea = document.getElementById("calc-result")
+    const textContent = outputTextArea.textContent
+    const lastChar = textContent.charAt(textContent.length - 1)
+    if (isOperator(text) && isOperator(lastChar)) {
+        return
+    }
     outputTextArea.textContent += text
 }
